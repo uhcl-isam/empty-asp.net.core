95c0611463c6db5d1f6125ac76b2f4ff2cfd4e8b
Michael Y Wu <wum@uhcl.edu>
2019-11-11 12:48:10 -0600

    Form Post and Get

---
+++ Website/Website/FormController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Website
+{
+    [Route("/form")]
+    public class FormController : Controller
+    {
+        [HttpGet]
+        [HttpPost]
+        [Produces("text/html")]
+        public IActionResult Index()
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("<!doctype html>");
+            sb.AppendLine("<html>");
+            sb.AppendLine("<head>");
+            sb.AppendLine("<link href='lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />");
+            sb.AppendLine("<link href='lib/bootstrap/dist/css/bootstrap-theme.css' rel='stylesheet' />");
+            sb.AppendLine("<body>");
+            sb.AppendLine("<div class='container'>");
+            sb.AppendLine("<table class='table table-striped table-hover'>");
+            if (HttpContext.Request.Query.Any())
+            {
+                sb.AppendLine("<tr class='success'><th colspan='2' class='alert alert-success'>Form GET</th></tr>");
+                AddRow(sb, "name", "value", true);
+                foreach (var kvp in HttpContext.Request.Query)
+                {
+                    AddRow(sb, kvp.Key, kvp.Value);
+                }
+            }
+            else
+            {
+                sb.AppendLine("<tr class='warning'><th colspan='2' class='alert alert-warning'>Form GET is Empty.</th></tr>");
+            }
+            if (Request.HasFormContentType && Request.Form != null && Request.Form.Any())
+            {
+                sb.AppendLine("<tr class='success'><th colspan='2' class='alert alert-success'>Form POST</th></tr>");
+                AddRow(sb, "name", "value", true);
+                foreach (var kvp in Request.Form)
+                {
+                    AddRow(sb, kvp.Key, kvp.Value);
+                }
+            }
+            else
+            {
+                sb.AppendLine("<tr class='warning'><th colspan='2' class='alert alert-warning'>Form POST is Empty.</th></tr>");
+            }
+            sb.AppendLine("</table>");
+
+            sb.AppendLine("</div>");
+            sb.AppendLine("</body>");
+            sb.AppendLine("</html>");
+            return Ok(sb.ToString());
+        }
+
+        private static void AddRow(StringBuilder sb, string name, string value, bool isHeader = false)
+        {
+            string tag = isHeader ? "th" : "td";
+            sb.Append("<tr>");
+            sb.AppendFormat("<{0}>{1}</{0}>", tag, name);
+            sb.AppendFormat("<{0}>{1}</{0}>", tag, value);
+            sb.AppendLine("</tr>");
+        }
+    }
+}
--- Website/Website/Startup.cs
+++ Website/Website/Startup.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,18 +21,50 @@ namespace EmptyWebSite
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.OutputFormatters.Add(new HtmlOutputFormatter()));
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDirectoryBrowser();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseDefaultFiles();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
+            app.UseDefaultFiles();
             app.UseDirectoryBrowser();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
+        }
+
+
+        public class HtmlOutputFormatter : StringOutputFormatter
+        {
+            public HtmlOutputFormatter()
+            {
+                SupportedMediaTypes.Add("text/html");
+            }
         }
     }
 }
